39adb609813148044ae85954935f0934d23b7241
Peter Vilja <peter.vilja@elisa.fi>
2013-02-24 22:17:46 +0200

    Tested different templates to figure out which is best for me

--- src/main/scala/fi/peter/todo/rest/TodoResource.scala
+++ src/main/scala/fi/peter/todo/rest/TodoResource.scala
@@ -3,11 +3,32 @@ package fi.peter.todo
 import org.scalatra._
 import scalate.ScalateSupport
 
-class TodoServlet extends ScaltaratodoStack {
+class TodoServlet extends ScalatraServlet with ScalateSupport {
 
-  get("/") {
-  	contentType = "application/json"
-    "{\"hello\":\"world\"}"
-  }
-  
+	override protected def defaultTemplatePath: List[String] = List("/templates/views")
+
+	get("/") {
+  		contentType="text/html"
+  		mustache("/index", "name" -> "scalatra, scalate and mustache")
+	}
+
+	get("/:to") {
+		contentType = "application/json"
+		"Hello " + params("to")
+  	}
+
+	get("/jade") {
+  		contentType = "text/html"
+  		jade("/hello-scalate", "foo" -> "bar")
+  	}
+
+  	get("/mustache") {
+  		contentType = "text/html"
+  		mustache("/index", "name" -> "mustache")
+  	}
+
+  	get("/ssp") {
+  		contentType = "text/html"
+  		ssp("/test")
+  	} 
 }
